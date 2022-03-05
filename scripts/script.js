5f055429e76e38323ca99faee565357266f81267
Julia <juliapsrk@gmail.com>
2022-03-05 12:26:26 +0100

    instruction screen and buttons added

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,33 @@
 const canvasElement = document.querySelector('canvas');
 
+const informationScreenElement = document.getElementById('information-screen');
+
 const startScreenElement = document.getElementById('start-screen');
 const playingScreenElement = document.getElementById('playing-screen');
 const endScreenElement = document.getElementById('game-over-screen');
 
-const startButton = startScreenElement.querySelector('button');
+const nextButton = informationScreenElement.querySelector('.nextButton');
+const backButton = startScreenElement.querySelector('.backButton');
+const startButton = startScreenElement.querySelector('.playButton');
 const pauseButton = playingScreenElement.querySelector('.pauseButton');
-const tryAgainButton = endScreenElement.querySelector('button');
+const tryAgainButton = endScreenElement.querySelector('.tryAgainButton');
 
 const screenElements = {
+  information: informationScreenElement,
   start: startScreenElement,
   play: playingScreenElement,
   end: endScreenElement
 };
 
 const game = new Game(canvasElement, screenElements);
+
+nextButton.addEventListener('click', () => {
+  game.displayScreen('start');
+});
+
+backButton.addEventListener('click', () => {
+  game.displayScreen('information');
+});
 
 startButton.addEventListener('click', () => {
   game.start();
